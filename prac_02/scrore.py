f549943fc0725ff6f3f5fac5b57cb67e5b46da1e
Vi Hoa <caovihoa@gmail.com>
2024-02-25 13:36:51 +0700

    fix score.py

--- prac_02/scrore.py
+++ prac_02/scrore.py
@@ -3,17 +3,31 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
+
 # TODO: Fix this!
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
 
-else:
-    print("Bad")
+def main():
+    score = float(input("Enter score: "))
+    result = get_result(score)
+    print_result(score, result)
+
+
+def get_result(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+def print_result(score, result):
+    print(f"Score: {score:.2f}, Result: {result}")
+
+
+main()

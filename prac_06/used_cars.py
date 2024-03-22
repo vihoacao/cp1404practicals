16878b2e3d64f0bd25b1b3be638da37985ca8bb5
Vi Hoa <caovihoa@gmail.com>
2024-03-22 12:12:01 +0700

    finish used_cars.py and car.py

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -6,13 +6,20 @@ This is why we name our folders with no spaces or capitals, as valid module name
 
 from prac_06.car import Car
 
+limo = Car(100)
+limo.add_fuel(20)
+print(f"Fuel in the car: {limo.fuel}")
+limo.drive(115)
+
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
-    my_car.drive(30)
-    print(f"Car has fuel: {my_car.fuel}")
+    my_car = Car("My Car", 180)
+    limo = Car("Limo", 100)
+    limo.add_fuel(20)
+    print(f"Fuel in limo: {limo.fuel}")
+    limo.drive(115)
     print(my_car)
-
+    print(limo)
 
 main()

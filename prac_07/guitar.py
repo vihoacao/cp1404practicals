3f2d519a443cd45539b41d1d397e7f90af11fbfb
Vi Hoa <caovihoa@gmail.com>
2024-03-24 16:52:42 +0700

    Complete first part of More Guitars exercise

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -7,9 +7,32 @@ class Guitar:
     def __str__(self):
         return "{} ({}) : ${:,.2f}".format(self.name, self.year, self.cost)
 
-    def get_age(self):
-        current_year = 2024
-        return current_year - self.year
+    def __lt__(self, other):
+        return self.year < other.year
 
-    def is_vintage(self):
-        return self.get_age() >= 50
+
+def get_guitars():
+    guitars = []
+    with open('guitars.csv', 'r') as file:
+        for line in file:
+            name, year, cost = line.strip().split(',')
+            guitars.append(Guitar(name, int(year), float(cost)))
+    return guitars
+
+
+def display_guitars(guitars):
+    print("Guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        print(f"Guitar {i}: {guitar}")
+
+
+def main():
+    guitars = get_guitars()
+    display_guitars(guitars)
+    print("\nSorting guitars by year...")
+    guitars.sort()
+    display_guitars(guitars)
+
+
+if __name__ == "__main__":
+    main()

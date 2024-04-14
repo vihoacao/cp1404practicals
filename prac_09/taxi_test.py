abe973d0f661e7f0d879cac07ea908f2fa89f88f
Vi Hoa <caovihoa@gmail.com>
2024-04-14 12:26:17 +0700

    change tax class (variable)

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -1,19 +1,19 @@
 from prac_09.taxi import Taxi
 
-# Create a new taxi object
-my_taxi = Taxi("Prius 1", fuel=100, price_per_km=1.23)
+# Create a new taxi object, my_taxi, with name "Prius 1" and 100 units of fuel
+my_taxi = Taxi("Prius 1", 100)
 
 # Drive the taxi 40 km
 my_taxi.drive(40)
 
 # Print the taxi's details and the current fare
 print(my_taxi)
-print(f"Current fare: ${my_taxi.get_fare():.2f}")
 
-# Restart the meter (start a new fare) and then drive the car 100 km
+# Restart the meter (start a new fare)
 my_taxi.start_fare()
+
+# Drive the car 100 km
 my_taxi.drive(100)
 
 # Print the details and the current fare
 print(my_taxi)
-print(f"Current fare: ${my_taxi.get_fare():.2f}")
